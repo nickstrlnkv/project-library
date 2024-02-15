9c200ec1282a382565bb04f3c2ee5194b3a62a8f
Nikita <98941217+nickstrlnkv@users.noreply.github.com>
2024-02-15 15:41:23 +0500

    Write a object for book

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(article, author, pages) {
+  // the constructor...
+  this.article = article;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+book1 = new Book('Harry Potter', 'J.K Rowling', 300);
+
+addBookToLibrary(book1);
